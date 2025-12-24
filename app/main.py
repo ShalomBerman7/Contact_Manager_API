c9b7829ccc08249ac082221304dae81d87d7260e
shalom berman 7 <bermanshalom7@gmail.com>
2025-12-24 12:19:58 +0200

    start build api

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
 from pathlib import Path
+from pydantic import BaseModel
+import uvicorn
+from data_interactor import Contact
 
 app = FastAPI()
 
 path = Path()
+
 
 class Item(BaseModel):
     first_name: str
@@ -14,8 +17,23 @@ class Item(BaseModel):
 
 @app.get("/items")
 def get_items():
+    items = Contact.get_all_contacts()
+    return items
+
+
+@app.post("/items")
+def post_item(first_name, last_name, phone_number):
+    pass
+
+
+@app.put("/items{id}")
+def put_item(id, first_name, last_name, phone_number):
+    pass
+
+@app.delete("/items{id}")
+def delete_item(id):
     pass
 
 
 if __name__ == "__main__":
-    pass
+    uvicorn.run(app, host="0.0.0.0", port=8001)
